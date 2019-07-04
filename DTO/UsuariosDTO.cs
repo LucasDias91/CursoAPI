d9ee1956e2fcfbb91292bc918daa21d9b0b87559
Lucas Dias <lucas.dias@magically.com.br>
2019-07-04 10:51:21 -0300

    Comentários DTO

--- DTO/UsuariosDTO.cs
+++ DTO/UsuariosDTO.cs
@@ -8,29 +8,35 @@ using System.Web;
 
 namespace CursoAPI.DTO
 {
+    //Nome da tabela do banco de dados 
     [Table("Usuarios")]
     public class UsuariosDTO
     {
         [Key]
         public int idusuario { get; set; }
 
+        //Campo usuario é obrigário  
         [Required
             (ErrorMessage = "usuario é obrigatório", AllowEmptyStrings = false)]
         public string usuario { get; set; }
 
+        //Campo senha é obrigária
         [Required
             (ErrorMessage = "senha é obrigatória", AllowEmptyStrings = false)]
         public string senha { get; set; }
 
+        //Campo nome é obrigária
         [Required
             (ErrorMessage = "nome é obrigatório", AllowEmptyStrings = false)]
         public string nome { get; set; }
 
+        //Verificando se é uma telefone válido.
         [RegularExpression
             (@"^1\d\d(\d\d)?$|^0800 ?\d{3} ?\d{4}$|^(\(0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d\) ?|0?([1-9a-zA-Z][0-9a-zA-Z])?[1-9]\d[ .-]?)?(9|9[ .-])?[2-9]\d{3}[ .-]?\d{4}$",
               ErrorMessage = "Telefone em formato inválido.")]
         public string telefone { get; set; }
 
+        //Verificando se é um email válido.
         [RegularExpression
             (@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", 
             ErrorMessage = "E-mail em formato inválido.")]
