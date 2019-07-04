33e0c07faacb0d777fab7c45a20a0834a5ed9819
Lucas Dias <lucas.dias@magically.com.br>
2019-07-04 11:03:27 -0300

    Comentários controller

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -12,12 +12,11 @@ namespace CursoAPI.Controllers
     public class UsuariosController : ApiController
     {
 
-        
         // Get - Lista todos os usuários. 
         [HttpGet] 
         public HttpResponseMessage getUsuario()
         {
-            // Criando menssagem.
+            // Inicializando menssagem.
             HttpResponseMessage _response = new HttpResponseMessage();
 
             //Tentando
@@ -33,7 +32,7 @@ namespace CursoAPI.Controllers
             //Pegando erro. 
             catch (Exception ex)
             {
-                //Criando response 
+                //Criando mesagem de erro 
                 _response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
 
@@ -41,86 +40,119 @@ namespace CursoAPI.Controllers
             return _response;
         }
 
+        // Get - Lista um usuário. 
         [HttpGet]
         public HttpResponseMessage getUsuario([FromUri] int idUsuario)
         {
+            // Inicializando mensagem.
             HttpResponseMessage _response = new HttpResponseMessage();
 
             try
             {
+                // Obtendo usuário pelo o id.
                 UsuariosDTO _usuario = (new UsuariosDAO().SelectUsuario(idUsuario));
+
+                // Criando response de sucesso com o usuário.
                 _response = Request.CreateResponse(HttpStatusCode.OK, _usuario);
             }
-            catch(Exception ex)
+            //Pegando erro. 
+            catch (Exception ex)
             {
+                //Criando mensagem de erro 
                 _response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
 
+            //Retornando mensagem. 
             return _response;
         }
 
+        // Post - Inserindo um novo usuário. 
         [HttpPost]
         public HttpResponseMessage postUsuario([FromBody] UsuariosDTO Usuario)
         {
+            // Inicializando mensagem.
             HttpResponseMessage _response = new HttpResponseMessage();
+
+            // Validando modelo de dados
             if (!ModelState.IsValid)
             {
+                //Criando error response se o modelo estiver inválido. 
                 _response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
                 return _response;
             }
 
             try
             {
+                //Inserindo usuário no banco de dados 
                 string msg = (new UsuariosDAO().InsertUsuario(Usuario));
+
+                //Criando mensagem de sucesso!
                 _response = Request.CreateResponse(HttpStatusCode.OK, msg);
             }
+            //Pegando error 
             catch (Exception ex)
             {
+                // Criando mensagem de erro!
                 _response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
 
+            //Retornando mensagem de sucesso!
             return _response;
         }
 
+        // Put - Alterando usuário. 
         [HttpPut]
         public HttpResponseMessage putUsuario([FromBody] UsuariosDTO Usuario)
         {
+            // Inicializando mensagem.
             HttpResponseMessage _response = new HttpResponseMessage();
 
+            // Validando modelo de dados
             if (!ModelState.IsValid)
             {
+                //Criando mensagem de error!
                 _response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
                 return _response;
             }
 
             try
             {
+                //Alterando usuário no banco de dados 
                 string msg = (new UsuariosDAO().UpdateUsuario(Usuario));
+                //Criando mensagem de sucesso!
                 _response = Request.CreateResponse(HttpStatusCode.OK, msg);
             }
             catch (Exception ex)
             {
+                //Criando mensagem de error!
                 _response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
 
+            //Retornando mensagem.
             return _response;
         }
 
+        //Delete - Deletando um usuário
         [HttpDelete]
         public HttpResponseMessage deleteUsuario([FromUri] int idUsuario)
         {
+            // Inicializando mensagem.
             HttpResponseMessage _response = new HttpResponseMessage();
 
             try
             {
+                //Deletando usuário do banco de dados 
                 string msg = (new UsuariosDAO().DeleteUsuario(idUsuario));
+                //Criando mensagem de sucesso!
                 _response = Request.CreateResponse(HttpStatusCode.OK, msg);
             }
+            //Pegando erro
             catch (Exception ex)
             {
+                //Criando mensagem de erro
                 _response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
-
+            //Retornando mensagem. 
             return _response;
         }
     }
